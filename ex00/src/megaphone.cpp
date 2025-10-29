aff37012a68475f4b39debfe11f08bce626ebd9a
Antoine Bouclier <abouclie@z4r2p4.42lyon.fr>
2025-10-29 13:19:08 +0100

    Upgrade: ex00

--- ex00/src/megaphone.cpp
+++ ex00/src/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: abouclie <abouclie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 09:48:15 by abouclie          #+#    #+#             */
-/*   Updated: 2025/10/29 08:44:20 by abouclie         ###   ########lyon.fr   */
+/*   Updated: 2025/10/29 13:17:46 by abouclie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 	std::string	str;
 	
 	if (argc == 1)
-		str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		for (int i = 1; i < argc; i++)
@@ -25,7 +25,8 @@ int main(int argc, char **argv)
 			for (int j = 0; argv[i][j]; j++)
 				str += static_cast<char>(std::toupper(argv[i][j]));
 		}
+		std::cout << str;
 	}
-	std::cout << str << std::endl;
+	std::cout << std::endl;
 	return (0);
 }
